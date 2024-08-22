e57f4436c4c55da29abd9d9904eb81d104967218
Taaakuu <fujiwarachris324@gmail.com>
2024-08-23 00:52:54 +0800

    编辑个人资料

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -23,16 +23,21 @@ class UsersController extends Controller
         return view('users.show', compact('user'));
     }
 
-
-
-    public function edit(User $user)
+    /**
+     * 显示用户注册页面
+     *
+     * @param User $user
+     * @return Factory|View|Application
+     */
+    public function edit(User $user): View|Factory|Application
     {
-
+        return view('users.edit', compact('user'));
     }
 
 
-    public function update(User $user)
+    public function update(UserRequest $request, User $user)
     {
-
+        $user->update($request->all());
+        return redirect()->route('users.show', $user->id)->with('success', '个人资料更新成功');
     }
 }
