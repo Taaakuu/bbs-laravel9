331725d7c85c91ffc1b75c7bc4c1b3f6b3aea864
Taaakuu <fujiwarachris324@gmail.com>
2024-08-24 23:38:33 +0800

    授权访问

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -5,21 +5,37 @@ namespace App\Http\Controllers;
 use App\Handlers\ImageUploadHandler;
 use App\Models\User;
 use Faker\Factory;
+use Illuminate\Auth\Access\AuthorizationException;
 use Illuminate\Console\Application;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\Contracts\View\View;
 
+/**
+ * Class UsersController
+ *
+ * @package App\Http\Controllers
+ */
 class UsersController extends Controller
 {
+    /**
+     *UsersController constructor.
+     */
+    public function __construct()
+    {
+        $this->middleware('auth', ['except' => [ 'show']]);
+    }
+
     /**
      * 显示用户个人信息页面
      *
      * @param User $user
      * @return Factory|View|Application
+     * @throws AuthorizationException
      */
     public function show(User $user): Factory|View|Application
     {
+        $this->authorize('update', $user);
         return view('users.show', compact('user'));
     }
 
@@ -42,9 +58,11 @@ class UsersController extends Controller
      * @param ImageUploadHandler $uploader
      * @param User $user
      * @return RedirectResponse
+     * @throws AuthorizationException
      */
     public function update(UserRequest $request, ImageUploadHandler $uploader, User $user): RedirectResponse
     {
+        $this->authorize('update', $user);
         $data = $request->all();
 
         if ($request->avatar) {
