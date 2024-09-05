f16cfa826bb6895535dbfa28d89bdb88bf8f1861
Taaakuu <fujiwarachris324@gmail.com>
2024-09-05 10:21:58 +0800

    后台内容管理

--- config/administrator.php
+++ config/administrator.php
@@ -45,6 +45,11 @@ return array(
             'roles',
             'permissions',
         ],
+        '内容管理' => [
+            'categories',
+            'topics',
+            'replies'
+        ]
     ],
 
     /*
