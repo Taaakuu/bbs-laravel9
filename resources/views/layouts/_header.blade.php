67a0250933f8568b6065498a6979f784acde596e
Taaakuu <fujiwarachris324@gmail.com>
2024-09-06 07:51:57 +0800

    修改更新

--- resources/views/layouts/_header.blade.php
+++ resources/views/layouts/_header.blade.php
@@ -58,20 +58,33 @@
                             {{ Auth::user()->name }}
                         </a>
                         <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                            <a class="dropdown-item" href="{{ route('users.show', Auth::id()) }}">个人中心</a>
-                            <a class="dropdown-item" href="{{ route('users.edit', Auth::id()) }}">编辑资料</a>
-                            <div class="dropdown-divider"></div>
-                            <a class="dropdown-item" id="logout" href="#">
-                                <form action="{{ route('logout') }}" method="POST"
-                                      onsubmit="return confirm('您确定要退出吗？')">
-                                    {{ csrf_field() }}
-                                    <button class="btn btn-block btn-danger" type="submit" name="button">退出</button>
-                                </form>
-                            </a>
-                        </div>
-                    </li>
-                @endguest
-            </ul>
+                            @can('manage_contents')
+                                <a class="dropdown-item" href="{{ url(config('administrator.uri')) }}">
+                                    <i class="fa-solid fa-gauge-high"></i>
+                                    管理后台
+                                </a>
+                                <div class="dropdown-divider"></div>
+                            @endcan
+                                <a class="dropdown-item" href="{{ route('users.show', Auth::id()) }}">
+                                    <i class="fa-solid fa-user"></i>
+                                    个人中心
+                                </a>
+                                <a class="dropdown-item" href="{{ route('users.edit', Auth::id()) }}">
+                                    <i class="fa-solid fa-pen-to-square"></i>
+                                    编辑资料
+                                </a>
+                                <div class="dropdown-divider"></div>
+                                <a class="dropdown-item" id="logout" href="#">
+                                    <form action="{{ route('logout') }}" method="POST"
+                                          onsubmit="return confirm('您确定要退出吗？')">
+                                        {{ csrf_field() }}
+                                        <button class="btn btn-block btn-danger" type="submit" name="button">退出</button>
+                                    </form>
+                                </a>
+                            </div>
+                        </li>
+                    @endguest
+                </ul>
+            </div>
         </div>
-    </div>
-</nav>
+    </nav>
